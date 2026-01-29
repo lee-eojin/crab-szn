fd9ecdb7e4dbdf34159b1c60260cc6c5414be02e
lee-eojin <lejin0906@snu.ac.kr>
2026-01-29 14:45:55 +0900

    chore: update gitignore for rust

--- baseball/src/main.rs
+++ baseball/src/main.rs
@@ -1,3 +1,12 @@
+use rand::seq::SliceRandom;
+
+fn generate_number() -> Vec<u8> {
+    let mut nums: Vec<u8> = (1..=9).collect();
+    nums.shuffle(&mut rand::thread_rng());
+    nums[0..3].to_vec()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let answer = generate_number();
+    println!("정답: {:?}", answer);
 }
